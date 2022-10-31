b58ebc47f60ed347f8521118538aacce0584eae0
Lucas da Silva Dias <lucasdias.ld10@gmail.com>
2022-10-31 17:49:44 -0300

    atualização requisição

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 function inicio() {
-    let menu1 = document.querySelector('.inicial');
-    menu1.classList.add('escondido');
-    let menu2 = document.querySelector('.principal');
-    menu2.classList.remove('escondido');
+
 
     let input = document.querySelector('.inicial input')
     let nome = input.value
@@ -11,5 +8,19 @@ function inicio() {
     const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', dados_nome )
     requisicao.then(Sucesso)
     requisicao.catch(Erro)
+
+    function Sucesso() {
+        console.log('sucesso')
+        let menu1 = document.querySelector('.inicial');
+        menu1.classList.add('escondido');
+        let menu2 = document.querySelector('.principal');
+        menu2.classList.remove('escondido');
+
+        const messages = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+    }
+
+    function Erro() {
+        console.log('erro')
+    }
 }
 
